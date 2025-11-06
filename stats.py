e3ed869c44c7e4398e0a56a4d971e4d41fb1f448
JoseCarlitosGordo <jose.gordo0806@gmail.com>
2025-11-06 20:45:05 +1030

    Add stats.py code

--- stats.py
+++ stats.py
@@ -3,11 +3,18 @@ def get_num_word(words_of_book):
     return len(list_words)
 
 def list_character_occurrence(words_of_book):
-    words_of_book = words_of_book.lower()
     list_characters = {}
-    for char in words_of_book:
+    for char in words_of_book.lower():
         if char not in list_characters:
-            list_characters[char] = 0
+            list_characters[char] = 1
         else:
             list_characters[char] += 1
-        return list_characters
+    return list_characters
+
+def sort_on(items):
+    return items["num"]
+def report(list_characters):
+    sorted_list = [{"char": key, "num": value} for key, value in list_characters.items()]
+    sorted_list.sort(key = sort_on, reverse = True)
+    return sorted_list
+
